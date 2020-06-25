ee0f1c93a8ad86e3440df639ad5c184d9a29fdbd
Cphil31 <cecilephilippe31@gmail.com>
2020-06-25 13:33:03 +0200

    first commit

---
+++ mail.php
@@ -0,0 +1,47 @@
+<?php
+
+function sendmail () {
+        
+    $subject = "Nouveau mail test";
+    //$prenom = $_POST['prenom'];
+    //$nom = $_POST['nom'];
+    $monEmail = "cecilephilippe31@gmail.com";
+    //$email_eleve = $_POST['log'];
+    //$formule = $_POST['id_formule'];
+
+    // $message 
+    $message = "Hello Philippe";
+
+
+    $content=" \n\n Message: $message ";
+    $destinataire = "cecilephilippe31@gmail.com";
+    $mailheader = "From: $monEmail ";
+
+
+    if (mail($destinataire, $subject, $content, $mailheader)) {
+        $sendmail = 'ok';
+        echo 'ok';
+
+    } else {
+        $sendmail = 'pas ok';
+        echo 'pas ok ';
+    }
+
+    return $sendmail;
+}
+
+sendmail();
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+</body>
+</html>
